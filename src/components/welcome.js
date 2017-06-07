a88acbfe8ae03702687c79ae29748b671b1ef0de
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-07 15:56:05 +0100

    Basic routing setup

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
-import Button from "react-native-button";
+import { Text, View } from 'react-native';
+import Button from 'react-native-button';
 
-const Welcome = () => {
+class Welcome extends React.Component {
+  static navigationOptions = {
+    title: 'Are you Drunk?'
+  };
+  render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, justifyContent: 'flex-end', backgroundColor: '#660066', borderBottomWidth: 0.5, borderColor: '#d6d7da', opacity: 0.8}}>
-          <Text style={{color: 'white', fontSize: 30, alignSelf: 'center'}}>ARE YOU DRUNK?</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: '#660066', borderBottomWidth: 0.5, borderColor: '#d6d7da', opacity: 0.8 }}>
+          <Text style={{ color: 'white', fontSize: 30, alignSelf: 'center' }}>ARE YOU DRUNK?</Text>
         </View>
-        <View style={{flex: 4, padding:10, backgroundColor: '#660066', opacity: 0.8}}>
-          <Text style={{color: 'white', fontSize: 30, alignSelf: 'center', marginBottom: 40}}>
+        <View style={{ flex: 4, padding:10, backgroundColor: '#660066', opacity: 0.8 }}>
+          <Text style={{ color: 'white', fontSize: 30, alignSelf: 'center', marginBottom: 40 }}>
             Welcome to the best drunk quiz app in the world. Are you ready?
           </Text>
-          <Button style={{color: '#660066', alignSelf: 'center', padding: 5, backgroundColor:'powderblue', borderWidth: 1, borderColor: '#000000', overflow: 'hidden', borderRadius: 15}}>
-            Begin
-          </Button>
+          <Button style={{ color: '#660066', alignSelf: 'center', padding: 5, backgroundColor:'powderblue', borderWidth: 1, borderColor: '#000000', overflow: 'hidden', borderRadius: 15 }}
+            onPress={() => navigate('Quiz')}
+            title="Quiz Start"
+          >Begin</Button>
         </View>
-        <View style={{flex: 4, backgroundColor: '#660066', opacity: 0.8}}>
-
+        <View style={{ flex: 4, backgroundColor: '#660066', opacity: 0.8 }}>
         </View>
       </View>
     );
   }
+}
 
-// Make the component available to other parts of the app
 export default Welcome;
