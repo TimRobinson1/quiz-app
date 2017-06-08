18d517476dcaef27bae40f4243a19f6c8da87887
Timothy Robinson <timmyrobby@gmail.com>
2017-06-08 11:00:45 +0100

    Extracts styles

--- src/components/quizquestions.js
+++ src/components/quizquestions.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, Image, Linking } from 'react-native';
 import Button from "react-native-button";
-// import styles from '../styles';
+import styles from '../styles';
 
 class QuizQuestions extends React.Component {
   static navigationOptions = {
@@ -11,21 +11,21 @@ class QuizQuestions extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <Image source={require('../quizbg.jpg')} style={{flex: 1, height:300, width:null}}>
-        <View style={{flex: 1, backgroundColor: 'rgba(102,0,102,0.5)'}}>
-          <View style={{flex: 4, padding: 40, justifyContent: 'flex-end'}}>
-            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 80, alignSelf: 'center', marginBottom: 40, textAlign: 'center' }}>
+        <View style={styles.background}>
+          <View style={styles.questionContainer}>
+            <Text style={styles.question}>
               Are you drunk?
             </Text>
           </View>
-          <View style={{flex: 4, padding: 40}}>
-              <Button onPress={() => navigate('Home')} style={{color: '#FFFFFF', margin:10, height:50, fontSize:40, backgroundColor:'purple', borderWidth: 1, borderColor: '#FFFFFF', overflow: 'hidden', borderRadius: 15}}>
+          <View style={styles.buttonContainer}>
+              <Button onPress={() => navigate('Home')} style={styles.quizButton}>
                 Yes
               </Button>
-              <Button onPress={() => navigate('Home')} style={{color: '#FFFFFF', margin:10, height:50, fontSize:40, backgroundColor:'purple', borderWidth: 1, borderColor: '#FFFFFF', overflow: 'hidden', borderRadius: 15}}>
+              <Button onPress={() => navigate('Home')} style={styles.quizButton}>
                 No
               </Button>
-              <Button onPress={() => navigate('Home')} style={{color: '#FFFFFF', margin:10, height:50, fontSize:40, backgroundColor:'purple', borderWidth: 1, borderColor: '#FFFFFF', overflow: 'hidden', borderRadius: 15}}>
-                How dare you
+              <Button onPress={() => navigate('Home')} style={styles.quizButton}>
+                Maybe
               </Button>
           </View>
         </View>
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, Image, Linking } from 'react-native';
 import Button from "react-native-button";
-// import styles from '../styles';
+import styles from '../styles';
 
 class Welcome extends React.Component {
   static navigationOptions = {
@@ -11,20 +11,20 @@ class Welcome extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <Image source={require('../welcome.jpg')} style={{flex: 1, height:300, width:null}}>
-        <View style={{flex: 1, backgroundColor: 'rgba(102,0,102,0.5)'}}>
-          <View style={{flex: 4, padding: 40, justifyContent: 'flex-end'}}>
-            <Text style={{color: 'white', fontSize: 35, alignSelf: 'center', marginBottom: 40, textAlign: 'center' }}>
+        <View style={styles.background}>
+          <View style={styles.questionContainer}>
+            <Text style={styles.greeting}>
               Are you over 18?
             </Text>
           </View>
-          <View style={{flex: 4, padding: 40}}>
-              <Button onPress={() => navigate('Quiz')} style={{color: '#FFFFFF', height:50, fontSize:40, backgroundColor:'purple', borderWidth: 1, borderColor: '#FFFFFF', overflow: 'hidden', borderRadius: 15}}>
+          <View style={styles.buttonContainer}>
+              <Button onPress={() => navigate('Quiz')} style={styles.button}>
                 Yes
               </Button>
           </View>
         </View>
-        <View style={{backgroundColor:'rgba(102,0,102,0.5)' }}>
-          <Text style={{color: '#538EED', textDecorationLine: 'underline', textAlign: 'center',fontSize: 20 }}
+        <View style={styles.drinkaware}>
+          <Text style={styles.link}
             onPress={() => Linking.openURL('http://drinkaware.co.uk')}>
             Drink Aware
           </Text>
