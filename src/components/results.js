86285819f725adb725555c486cd67d25663e8986
Timothy Robinson <timmyrobby@gmail.com>
2017-06-09 14:29:47 +0100

    Adds background results images

--- src/components/results.js
+++ src/components/results.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, View, Image, Linking } from 'react-native';
 import Button from "react-native-button";
 import styles from '../styles';
-import { calculate } from '../helpers/functions'
+import { calculate, resultImage } from '../helpers/functions'
 
 class Results extends React.Component {
   static navigationOptions = {
     title: 'Project Guinness'
   };
   render() {
-    const score = this.props.navigation.state.params.score
+    const score = this.props.navigation.state.params.score;
     return (
-      <Image source={require('../quizbg.jpg')} style={{flex: 1, height:300, width:null}}>
+      <Image source={resultImage(score)} style={{flex:1, height:50, width:null}}>
         <View style={styles.background}>
           <View style={styles.questionContainer}>
             <Text className='result' style={styles.result}>
               { calculate(score) }
             </Text>
+          </View>
+          <View>
           </View>
           <View style={styles.buttonContainer}>
               <Button className='button' onPress={() => this.props.navigation.navigate('Home')} style={styles.quizButton}>
